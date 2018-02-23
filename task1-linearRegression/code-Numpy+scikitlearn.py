53f1972158a82b8012b87bb1722831356225ab65
Haihan-W <33006435+Haihan-W@users.noreply.github.com>
2018-02-23 16:23:10 -0700

    Using different method for multi-linear regression V1
    https://www.cnblogs.com/pinard/p/6016029.html

--- task1-linearRegression/code-Numpy+scikitlearn.py
+++ task1-linearRegression/code-Numpy+scikitlearn.py
@@ -1,18 +1,23 @@
+#Tutorial: https://www.cnblogs.com/pinard/p/6016029.html
+
 '''Data Preprocessing--Importing libraries'''
 import numpy as np #for array manipulation'
 import pandas as pd #for providing data structures--e.g. import dataset from online to be the input of the program'
 import matplotlib.pyplot as plt #Python2D plotting library'
+from sklearn import datasets, linear_model
 
 '''Data Preprocessing--Importing dataset'''
 dataset=pd.read_csv('/home/whh/Ubuntu/task1-linearRegression/kc_house_data.csv') #file pathway. import csv file
-#dataset.columns = ['col1','col2'] #give them col(variable) names
-#dateset.head() #just for a preview:this can display data into a table with column name created above using .columns
+#just for a preview: read first 5 lines of data. If read the last 5 lines, use data.tail()
+dateset.head() 
+#check dimension of dataset: result will be (#rows,#columns)
+dataset.shape
 
-#assume in csv file provided, the last column is Dependent Variable--y, the rest columns are indep variables--X. 
-X=dataset.iloc[:,3].values #X is a multi-col array; Note:iloc is a PANDA function, for selection by position/index
-X=X.reshape(-1,1)
-y=dataset.iloc[:,2].values
-print (X,y)
+'''Data Preprocessing--Define dependent variable--y and independent variables and put them into one vector--X''' 
+X=dataset[['AT','V','AP','RH']]
+x.head()
+y=dataset[['PE']]
+y.head()
 
 '''Data Preprocessing--Taking care of missing data
 #note: some data is missing and make the program invalid to run
@@ -25,29 +30,27 @@ X[:,..:..]=imputer.transform(X[:,..:..]) #missed data is replaced in the corresp
 '''----------------DATA PREPROCESSING above, come from this tutorial-----------------https://www.udemy.com/deeplearning/learn/v4/t/lecture/6745258?start=0 This lecture section 29-- step 1-3
 '''
 
-
-
+'''Separate dataset to 'training set'--for generating model and 'test set'--for testing model '''
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+print X_train.shape
+print y_train.shape
+print X_test.shape
+print y_test.shape
 
 '''Linear regression on the data'''
 from sklearn.linear_model import LinearRegression 
-
-from sklearn.preprocessing import scale
-
 reg=LinearRegression()
-reg.fit(X,y)
-slope=reg.coef_(0)
-intercept=reg.intercept_
+reg.fit(X_train,y_train)
+print reg.coef_ 
+print reg.intercept_
 
-#plot-- For one indep vari X only. (NO x1,x2... in X matrix)
-plt.scatter(X,y,color='black')
-predicted_values=[slope * i + intercept for i in X]
-plt.plot(X,predicted_values,'b')
-plt.xlabel("name of x")
-plt.ylabel("name of y")
-
-#IF want to predict y given x,e.g.: 
-reg.predict(X=6.2)
-
+'''Evaluate model:''' 
+y_pred=reg.predict(X_test)
+#calculate MSE and RMSE -- the lower the better
+from sklearn import metrics
+print "MSE:",metrics.mean_squared_error(y_test,y_pred)
+print "RMSE:",np.sqrt(metrics,mean_squared_error(y_test,y_pred))
 
 
 
